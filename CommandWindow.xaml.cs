3a3d80eadeb0fe21baf1e22c3928bcdce2f0edd3
JakeMertel <jake.mertel@local>
2026-01-11 21:57:27 -0600

    input fix

--- CommandWindow.xaml.cs
+++ CommandWindow.xaml.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace GridBanner
 {
     public partial class CommandWindow : Window
     {
         public event EventHandler<string>? CommandExecuted;
+        private string _commandText = string.Empty;
 
         public CommandWindow()
         {
@@ -20,8 +22,21 @@ namespace GridBanner
             Activate();
             Topmost = true;
             Focus();
+            
+            // Force set colors using multiple methods to ensure visibility
+            var blackBrush = new SolidColorBrush(Colors.Black);
+            var whiteBrush = new SolidColorBrush(Colors.White);
+            
+            CommandTextBox.Foreground = blackBrush;
+            CommandTextBox.Background = whiteBrush;
+            CommandTextBox.CaretBrush = blackBrush;
+            CommandTextBox.BorderBrush = new SolidColorBrush(Color.FromRgb(0, 120, 212));
+            CommandTextBox.BorderThickness = new Thickness(1);
+            
+            // Set text directly
+            CommandTextBox.Text = string.Empty;
+            
             CommandTextBox.Focus();
-            CommandTextBox.SelectAll();
         }
 
         private void CommandTextBox_KeyDown(object sender, KeyEventArgs e)
@@ -59,4 +74,3 @@ namespace GridBanner
         }
     }
 }
-
